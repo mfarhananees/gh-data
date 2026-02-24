8d3ad98bc1e4a569001bce4932ec69715140ff89
mfarhananees <mfarhan.anees@hotmail.com>
2026-02-24 15:15:34 +0500

    initial commit

---
+++ src/components/MainContent.test.jsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import MainContent from './MainContent';
+
+//this is a comment to check if the workflow skipped with [skip ci]
+// this comment to check if the workflow skipped with [ci skip]
+describe('MainContent', () => {
+  it('should render a button', () => {
+    render(<MainContent />);
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('should show the help area after clicking the button', async () => {
+    render(<MainContent />);
+
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
+  });
+});
